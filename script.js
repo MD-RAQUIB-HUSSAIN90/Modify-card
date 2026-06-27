d0765799de80514efc6a2fea141100106762e542
MD RAQUIB HUSSAIN <mdraquibhussain71@gmail.com>
2026-06-28 02:09:56 +0530

    last commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let addNote = document.querySelector("#add-note");
+let formContainer = document.querySelector(".form-container");
+addNote.addEventListener("click", () => {
+  formContainer.style.display = "initial";
+  
+});
