9a12ec437ec59beef652c5fa2df59c954f85954e
MD RAQUIB HUSSAIN <mdraquibhussain71@gmail.com>
2026-07-04 23:26:29 +0530

    Remove event listener for adding notes

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
 let addNote = document.querySelector("#add-note");
 let formContainer = document.querySelector(".form-container");
-addNote.addEventListener("click", () => {
-  formContainer.style.display = "initial";
-  
-});
+
